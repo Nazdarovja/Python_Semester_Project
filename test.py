d73754dfd5e401505462656c81dc94716aae7533
mathias bigler <orchidevej@hotmail.com>
2018-11-30 12:45:00 +0000

    hey

--- test.py
+++ test.py
@@ -21,6 +21,7 @@ def clean_data(sentence):
     lyrics = lyrics.replace("*","")
     return sentence.lower()
 
+# Cleaning stop words for each song's lyrics
 def clean_puta_words(lyrics):
     for word in lyrics.split(): 
         if word in en_stops: # makes it O(1) because it's a Set (unique values), just like hashamp
@@ -34,6 +35,7 @@ def count_words(lyrics):
 
 # cleaning all songs' lyrics
 dataframe.lyrics.apply(lambda lyrics: clean_data(lyrics))
+dataframe.lyrics.apply(lambda lyrics: clean_puta_words(lyrics))
 
 # making a new column for each song and counts the most used words for each song
 dataframe['most_used_words'] = pd.Series(count_words(lyrics) for _, lyrics in dataframe['lyrics'].iteritems()])
