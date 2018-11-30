23787900832287928af6ed1f9353bb5cea2adf64
Stanislav Novitski <stani2980@hotmail.com>
2018-11-30 12:43:18 +0100

    added logic to not unzip if already unzipped

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from src.data.make_dataset import create_dataset
 
-
-
 if __name__ == "__main__":
     create_dataset()
--- src/data/util.py
+++ src/data/util.py
@@ -1,9 +1,17 @@
 import zipfile
-
+import os.path
 file_name = 'lyrics.csv.zip'
 path_to_zip_file = f'data/raw/{file_name}'
 directory_to_extract_to = f'data/external/{file_name}'
 
 def unzip_file():
-    with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
-        zip_ref.extractall(directory_to_extract_to)
+    """
+        If not done yet unzips the raw file and adds to external folder
+
+    """
+    if os.path.isfile(f'{directory_to_extract_to}/lyrics.csv'):
+        print('Skipping unzip...')
+    else:
+        print('Unzipping file...')
+        with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+            zip_ref.extractall(directory_to_extract_to)
