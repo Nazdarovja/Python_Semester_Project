f8bc770b21ad020c7ece845848e37550302f8c23
awha86 <awha86@gmail.com>
2018-11-30 12:33:54 +0100

    util method added; detect language

--- src/data/util.py
+++ src/data/util.py
@@ -1,4 +1,5 @@
 import zipfile
+from langdetect import detect
 
 file_name = 'lyrics.csv.zip'
 path_to_zip_file = f'data/raw/{file_name}'
@@ -7,3 +8,9 @@ directory_to_extract_to = f'data/external/{file_name}'
 def unzip_file():
     with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
         zip_ref.extractall(directory_to_extract_to)
+
+def detect_english_string(input_string):
+    '''
+    Detect the language of the text. Returns 'en' for english
+    '''
+    return detect(input_string) == 'en'
