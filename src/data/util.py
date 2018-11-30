43c547f7b1996293a302ec5ac6926fdd72e44c4b
Stanislav Novitski <stani2980@hotmail.com>
2018-11-30 12:25:16 +0100

    added license

--- src/data/util.py
+++ src/data/util.py
@@ -2,7 +2,7 @@ import zipfile
 
 file_name = 'lyrics.csv.zip'
 path_to_zip_file = f'data/raw/{file_name}'
-directory_to_extract_to = f'data/external/{file_name}'
+directory_to_extract_to = f'data/external/'
 
 def unzip_file():
     with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
