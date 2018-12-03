3595c63c2226e570dcddaea2a8b0f0087fffebcc
mathias bigler <orchidevej@hotmail.com>
2018-12-03 20:34:14 +0000

    heyoooo

--- main.py
+++ main.py
@@ -4,49 +4,35 @@ import numpy as np
 from src.data.make_dataset import create_dataset
 from src.data.util import _clean_lyrics,_detect_english_string, clean_puta_words
 
+def count_words_in_genre(genre, lyrics_df):
+    arr = np.array(lyrics_df[lyrics_df['genre'] == genre]['most_used_words'].tolist()) # merges row's most_used_word column to list
+    arr = arr[~pd.isna(arr)] # removing nans'
+    flat_list = [item for sublist in arr for item in sublist] # converts array of arrays to one big array
+    genre_dict = {}
+    for tupl in flat_list: # sums up total occurances of each word
+        genre_dict[tupl[0]] = genre_dict.get(tupl[0], 0) + tupl[1]
+    return genre_dict
+
 
 if __name__ == "__main__":
     # Creating the dataset, unzip, 
     lyrics_df = create_dataset()
-    print(len(lyrics_df))
-    lyrics_df = lyrics_df[lyrics_df['lyrics'].str.len() > 500] # making sure it's actually a "song"
-    lyrics_df = lyrics_df[(lyrics_df['genre'] != 'Not Available') & (lyrics_df['genre'] != 'Other')] # irrelevant genres
-    print(len(lyrics_df))
 
-    # lyrics_df = lyrics_df[90:100].reset_index()
-    # print(len(lyrics_df.iloc[0].lyrics))
-    # lyrics_df.lyrics.apply(lambda lyrics: _clean_lyrics(lyrics))
+    lyrics_df = lyrics_df[(lyrics_df['genre'] != 'Not Available') & (lyrics_df['genre'] != 'Other')] # irrelevant genres
 
     lyrics_df['lyrics'] = lyrics_df['lyrics'].apply(lambda x: clean_puta_words(x)) # cleaning the dataset for puta words
     lyrics_df['most_used_words'] = pd.Series(collections.Counter(lyrics.split())
                                     .most_common(10) for _, lyrics in lyrics_df['lyrics'].iteritems())
                                     # making a new column for each song and counts the most used words for each song
 
-    arr = np.array(lyrics_df[lyrics_df['genre'] == 'Rock']['most_used_words'].tolist())
-    arr = arr[~pd.isna(arr)]
-    arr = arr.flatten()
-    rock = {}
-
-    for x in arr:
-        for y in x:
-            if y[0] in rock:
-                rock[y[0]] += y[1]
-            else:
-                rock[y[0]] = y[1]
-            
-    # rock_most_used_words = {}
-    # for word in arr:
-    #     word = str(word).split(' ')
-    #     if word[0] in rock_most_used_words:
-    #         rock_most_used_words[word[0]] += word[1]
-    #     else:
-    #         rock_most_used_words[word[0]] = word[1]
+    words_genre = count_words_in_genre('Pop', lyrics_df)
 
     genres = lyrics_df['genre'].groupby(lyrics_df['genre']).count() # groups the dataset by genre and counts the amount of each genre
     print(lyrics_df.head()['most_used_words'])
     print(genres)
-    word_counter = collections.Counter(rock)
+    word_counter = collections.Counter(words_genre)
     print(word_counter.most_common(20))
+
 # What is the minimum length of the lyrics in order to be defined as a song? hmm
 # minimum 500 length and removal of 'Not Available' 
 # and 'Other' filter 61000 elements away from the dataset
--- src/data/util.py
+++ src/data/util.py
@@ -88,7 +88,7 @@ def _clean_lyrics(lyrics):
             Returns None if empty string
             
     """
-    if len(lyrics) > 1:
+    if len(lyrics) > 500:
         lyrics = lyrics.replace(",","")
         lyrics = lyrics.replace(".","")
         lyrics = lyrics.replace(":","")
@@ -129,7 +129,7 @@ def _detect_english_string(input_string, language= 'en'):
     return val
 
 def clean_puta_words(lyrics):
-    stop_words = ['i', 'like', 'me', 'you', 'it', "it's", 'too', 'to', 'nan']
+    stop_words = ['i', 'like', 'me', 'you', 'it', "it's", 'too', 'to', 'nan', 'the', 'and', 'a']
     words = lyrics.split()
     for idx, word in enumerate(words): 
         if word in stop_words: # makes it O(1) because it's a Set (unique values), just like hashamp
