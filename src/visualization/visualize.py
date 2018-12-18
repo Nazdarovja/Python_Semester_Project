18fee5a9d3d6c7e1b448f5adecd7e7629121f0ff
Mathias <Orchidevej@hotmail.com>
2018-12-18 10:59:01 +0100

    heyooo

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -4,18 +4,12 @@ from textblob import TextBlob
 from src.features.build_features import normalize, word_count, sentence_avg_word_length
 from src.features.text_blob_analysis import analyze_sentiment, analyze_word_class
 
-genre_dict = {
-    'g':'Rock',
-    'b':'Hip-Hop',
-    'r':'Pop'
-}
-
 def plotting(df):
-    # plot_genre_and_word_count(df)
-    # plot_genre_and_avg_word_len(df)
-    # plot_sentiment_analysis(df)
-    # plot_genre_and_normalized_word_count(df)
-    plot_word_classes(df)
+    plot_genre_and_word_count(df)
+    plot_genre_and_avg_word_len(df)
+    plot_sentiment_analysis(df)
+    plot_genre_and_normalized_word_count(df)
+    plot_word_class_pr_genre(df)
 
 def plot_genre_and_word_count(df):
     """
@@ -101,17 +95,51 @@ def plot_sentiment_analysis(df):
     plt.legend()
     plt.show()
 
-def plot_word_classes(df):
-    df = analyze_word_class(df)
-    plotting_helper_method('nouns', 'genre', df)
+def plot_word_class_pr_genre(df):
+    """
+    plots sentiment analysis.
+    Parameters
+        ----------
+        df : Dataframe
+            Pandas Dataframe with our filtered songs
+        Returns
+            Generates three graph with respectively nouns, verbs and adverbs on the x axis and then genre on the y axis
+            with different colors representing each genre.
+    """
 
+    df = analyze_word_class(df)
+
+    # plotting nouns
+    plotting_helper_method('nouns', 'genre', df)
     plt.title('Amount of nouns pr song pr. genre')
     plt.xlabel('Nouns')
     plt.ylabel('Genre')
     plt.legend()
     plt.show()
 
+    # plotting verbs
+    plotting_helper_method('verbs', 'genre', df)
+    plt.title('Amount of verbs pr song pr. genre')
+    plt.xlabel('Verbs')
+    plt.ylabel('Genre')
+    plt.legend()
+    plt.show()
+
+    # plotting adverbs
+    plotting_helper_method('adverbs', 'genre', df)
+    plt.title('Amount of adverbs pr song pr. genre')
+    plt.xlabel('Adverbs')
+    plt.ylabel('Genre')
+    plt.legend()
+    plt.show()
+    
+
 def plotting_helper_method(x_axis, y_axis, df):
+    genre_dict = {
+        'g':'Rock',
+        'b':'Hip-Hop',
+        'r':'Pop'
+    }
     for color, genre in genre_dict.items():
         filtered_df = df[df['genre'] == genre]
         plt.scatter(filtered_df[x_axis], filtered_df[y_axis], c=color, label=genre)
